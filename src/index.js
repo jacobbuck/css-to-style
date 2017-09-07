50ece1f9ac35a030704663adc1cffa3e7aa51371
Jacob Buck <buck.jacob@gmail.com>
2017-09-07 21:22:39 +1200

    Rewrite parser to not use regexps! And handles semi-colons in quotes!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 const camelCase = str => str.replace(/\-(\w|$)/g, (m, p1) => p1.toUpperCase());
 
-const cssToStyle = cssText =>
-  cssText.split(/;(?=[^\)]*(?:\(|$))/).reduce((styles, rule) => {
+const cssToStyle = cssText => {
+  const declarations = [];
+  let capturing;
+  let i = cssText.length;
+  let last = i;
+
+  // Split into declarations by semi-colon (outside quotes or parentheses)
+  while (i-- > -1) {
+    // Capture unescaped quotes
+    if ((cssText[i] === '"' || cssText[i] === "'") && cssText[i - 1] !== "\\") {
+      if (!capturing) {
+        capturing = cssText[i];
+      } else if (cssText[i] === capturing) {
+        capturing = false;
+      }
+    }
+    // Start capturing parentheses
+    if (!capturing && cssText[i] === ")") {
+      capturing = cssText[i];
+    }
+    // Stop capturing parentheses
+    if (cssText[i] === "(" && capturing === ")") {
+      capturing = false;
+    }
+    // Split at semi-colon
+    if (i < 0 || (!capturing && cssText[i] === ";")) {
+      declarations.unshift(cssText.slice(i + 1, last));
+      last = i;
+    }
+  }
+
+  return declarations.reduce((styles, rule) => {
     const i = rule.indexOf(":");
+    const value = rule.substr(i + 1).trim();
     let prop = rule
       .substr(0, i)
-      .trim()
-      .toLowerCase();
-    const value = rule.substr(i + 1).trim();
+      .toLowerCase()
+      .trim();
     if (prop && value) {
       if (prop === "float") {
         prop = "cssFloat";
@@ -18,5 +48,6 @@ const cssToStyle = cssText =>
     }
     return styles;
   }, {});
+};
 
 export default cssToStyle;
