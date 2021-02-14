638c245fec7cd93336300e9d307b43fa3b404418
Jacob Buck <buck.jacob@gmail.com>
2021-02-14 22:37:27 +0000

    Get file paths from package.json in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,17 @@
 import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [
     {
       exports: 'default',
-      file: 'lib/index.cjs.js',
+      file: pkg.main,
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: 'lib/index.esm.js',
+      file: pkg.module,
       format: 'esm',
       sourcemap: true,
     },
